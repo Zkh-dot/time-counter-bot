04f3ed6b3a1ba04cba623f3eee027bd9c71f5798
Andrey Tsinin <andrew.tsinin@gmail.com>
2025-02-11 12:19:42 +0200

    fix

--- db/init.go
+++ db/init.go
@@ -7,6 +7,7 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 // DB — глобальный объект для работы с базой через GORM.
@@ -20,7 +21,9 @@ func InitDB() {
 	}
 
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		log.Fatal("PostgreSQL connection error:", err)
 	}
